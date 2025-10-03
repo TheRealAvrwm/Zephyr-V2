db47e9f5790643a84ddc2ec338e042dc5efe5566
TheRealAvrwm <161508444+TheRealAvrwm@users.noreply.github.com>
2025-10-03 11:30:34 -0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -3,11 +3,17 @@ local player = Players.LocalPlayer
 local placeId = game.PlaceId
 
 if placeId == 75366259315586 then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/TheRealAvrwm/Zephyr-V2/refs/heads/main/Games/Build%20ur%20base.lua'), true))()
-elseif placeId == 17516596118 or placeId == 108428559529058 or placeId == 122181485177067 or placeId == 108428559529058 or placeId == 100040622766961 or placeId == 5995470825 then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/TheRealAvrwm/Zephyr-V2/refs/heads/main/Hypershot.lua'), true))()
-elseif placeId == 12355337193 or placeId == 13771457545 then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/TheRealAvrwm/Zephyr-V2/refs/heads/main/ZephyrV2'), true))()
-elseif placeId == 109983668079237 or placeId == 96342491571673 then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/TheRealAvrwm/Zephyr-V2/refs/heads/main/Games/StealABrainrot'), true))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/TheRealAvrwm/Zephyr-V2/refs/heads/main/Games/Build%20ur%20base.lua', true))()
+elseif placeId == 17516596118 
+    or placeId == 108428559529058 
+    or placeId == 122181485177067 
+    or placeId == 100040622766961 
+    or placeId == 5995470825 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/TheRealAvrwm/Zephyr-V2/refs/heads/main/Hypershot.lua', true))()
+elseif placeId == 12355337193 
+    or placeId == 13771457545 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/TheRealAvrwm/Zephyr-V2/refs/heads/main/ZephyrV2', true))()
+elseif placeId == 109983668079237 
+    or placeId == 96342491571673 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/TheRealAvrwm/Zephyr-V2/refs/heads/main/Games/StealABrainrot', true))()
 end
